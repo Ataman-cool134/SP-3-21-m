671a56d06c322895833df6dc1997788a2e990305
ataman-cool134 <lil_froze04@mail.ru>
2024-01-25 10:35:11 +0300

    выполнили работу с многомерными массивами

---
+++ Задание 3/app.js
@@ -0,0 +1,4 @@
+let apple = [['iphone 5s', 'iphone 6','iphone 7', 'iphone 8', 'iphone x'],['MacBook pro 10', 'MacBook pro 11','MacBook pro 12',
+'MacBook pro 13', 'MacBook pro 14'], ['ipad 4', 'ipad 5', 'ipad mini 8', 'ipad pro x', 'ipad 11'], ['apple watch', 'apple watch 2', 'apple watch 3', 'apple watch pro 2', 'apple watch pro max 3']];
+console.log(apple);
+console.log(apple[0][3]);
