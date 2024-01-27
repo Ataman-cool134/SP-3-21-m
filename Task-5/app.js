bd666a8b25c52c793a320dd5af0f871e2ff30444
EvelinaK1 <evelinago.pro@gmail.com>
2024-01-27 16:44:11 +0300

    выполнили задание с двумерными массивами

--- Task-5/app.js
+++ Task-5/app.js
@@ -1,5 +1,14 @@
-const arr=[1,2,3,4,5,6,7]
-for(let i=0;i<=arr.length;i++){
-    let result = arr[i]
-    console.log(result)
+// const arr=[1,2,3,4,5,6,7]
+// for(let i=0;i<=arr.length;i++){
+//     let result = arr[i]
+//     console.log(result)
+// }
+let arr=[['tom',39,1,3], ['bob',18,1,3],['stan',17,1,3]  ,['stan',17,1,3] ];
+for (let i = 0; i < arr.length ; i++) {
+    for (let j = 0; j < arr.length; j++){
+        let result = arr[i][j];
+        console.log(result);
+
+    }
+    console.log('-------------------');
 }
